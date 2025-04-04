ebb5986ba465a2bbdd184e658ac5ac973b608f2e
Stéphane Payrard <cognominal@gmail.com>
2025-04-04 12:08:26 +0200

    initial code

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import "../app.css";
+  
+  let { children } = $props();
+
+  import { AppBar } from "@skeletonlabs/skeleton-svelte";
+  import { ArrowLeft } from "lucide-svelte";
+  import { Paperclip } from "lucide-svelte";
+  import { Calendar } from "lucide-svelte";
+  import { CircleUser } from "lucide-svelte";
+</script>
+
+<div class="sticky top-0 z-10 bg-red-500 backdrop-blur-sm">
+  <AppBar>
+    {#snippet lead()}
+      <ArrowLeft size={24} />
+    {/snippet}
+    {#snippet trail()}
+      <Paperclip size={20} />
+      <Calendar size={20} />
+      <CircleUser size={20} />
+    {/snippet}
+    {#snippet headline()}
+      <h2 class="h2">Headline</h2>
+    {/snippet}
+  </AppBar>
+</div>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  /* @tailwind */
+  import { SplitPane } from "@rich_harris/svelte-split-pane";
+  let { data } = $props();
+</script>
+
+<div class="h-[100vh]">
+  <SplitPane
+    type="vertical"
+    id="main-split"
+    min="200px"
+    max="-200px"
+    pos="50%"
+    --color="#e5e7eb"
+    --thickness="4px"
+  >
+    {#snippet a()}
+      <!-- <div class="h-full p-4 overflow-y-auto"> -->
+      <div style="height: 100%; overflow-y: auto;" class="p-4">
+        {#each Array(200) as _, i}
+          <div class="mb-2 p-2 text-black bg-gray-100 rounded">
+            Line {i + 1}: Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.
+          </div>
+        {/each}
+      </div>
+    {/snippet}
+
+    {#snippet b()}
+      <div style="height: 100%; overflow-y: auto;" class="p-4">
+        {#each Array(200) as _, i}
+          <div class="mb-2 p-2 text-black bg-blue-50 rounded">
+            Line {i + 1}: Sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua.
+          </div>
+        {/each}
+      </div>
+    {/snippet}
+  </SplitPane>
+</div>
